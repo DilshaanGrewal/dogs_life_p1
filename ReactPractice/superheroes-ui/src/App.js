9c31dabc719713a0520001d289f351ae624b0e4c
Dilshaan Grewal <dilshaan.grewal.001@gmail.com>
2023-07-31 15:45:46 +0100

    ql3 - done task 3

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import HeroDetail from './components/HeroDetail';
+import './App.css';
+import AllHeroes from './components/AllHeroes';
 
 function App() {
   return (
-    <HeroDetail/>
+    <AllHeroes/>
   );
 }
 
--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import batman from './images/batman.jpeg'
+// import batman from '../images/batman.jpeg'
+import images from '..'
 
-const HeroDetail = () => {
+const HeroDetail = (props) => {
   return (
     <div>
-        <img src={batman} alt="Batman" height="250px"/>
-        <p id="blue-alias">Alias: Batman</p>
-        <p class="green-class">Name: Bruce Wayne</p>
+        {/* <img src={batman} alt="Batman" height="250px"/> */}
+        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
+        <p id="blue-alias">Alias: {props.info.alias}</p>
+        <p class="green-class">Name: {props.info.name}</p>
         <button class="green-class">Like</button>   
     </div>
   )
