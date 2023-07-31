4b05bb024f50e89c309056564f0241aad9449843
unknown <calinpav21@gmail.com>
2023-07-31 17:06:22 +0100

    ql-3

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import HeroDetail from './components/HeroDetail';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-      <HeroDetail/>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Heroes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">All Heroes</Nav.Link>
+            <Nav.Link href="#link">Add Hero</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
