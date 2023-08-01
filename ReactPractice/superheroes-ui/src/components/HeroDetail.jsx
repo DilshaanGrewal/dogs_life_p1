ca0c0574ebce7582162b0187ee8f2cb79e335696
Dilshaan Grewal <dilshaan.grewal.001@gmail.com>
2023-08-01 10:13:47 +0100

    ql-5

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-// import batman from '../images/batman.jpeg'
+import React, { useState } from 'react'
 import images from '../index'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 const HeroDetail = (props) => {
-  return (
-    // <div>
-    //     {/* <img src={batman} alt="Batman" height="250px"/> */}
-    //     <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
-    //     <p id="blue-alias">Alias: {props.info.alias}</p>
-    //     <p class="green-class">Name: {props.info.name}</p>
-    //     <button class="green-class">Like</button>   
-    // </div>
 
+  const[counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
+
+  return (
     <Card >
       <div className="imgContainer">
         <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
@@ -21,7 +23,12 @@ const HeroDetail = (props) => {
       <Card.Body >
         <Card.Title >Alias: {props.info.alias}</Card.Title>
         <Card.Text >Name: {props.info.name}</Card.Text>
-        <Button variant="primary">Like</Button>
+        {/* <Card.Text>Likes: {counter}</Card.Text> */}
+        {/* <Button variant="primary" onClick={incrementCounter}>Like</Button> */}
+        <Row>
+          <Col><Button variant='primary' onClick={incrementCounter}>Like</Button></Col>
+          <Col>{counter > 0 && <Card.Text>Likes: {counter}</Card.Text>}</Col>
+        </Row>
       </Card.Body>
     </Card>
   )
